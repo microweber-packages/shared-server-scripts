e0349ef728cb17b78460386824495060ef52203e
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2022-06-13 13:39:10 +0300

    Create MicroweberDownloaderTest.php

---
+++ tests/MicroweberDownloaderTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MicroweberPackages\SharedServerScripts;
+
+use PHPUnit\Framework\TestCase;
+
+class MicroweberDownloaderTest extends TestCase
+{
+    public function testDownload()
+    {
+
+    }
+
+}
